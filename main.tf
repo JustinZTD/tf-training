6c76c1b1a52c71847a165d65de16af3476b7ef2c
JustinZTD <zhangtianda90@gmail.com>
2025-04-15 10:56:17 +0900

    add pet 4

--- main.tf
+++ main.tf
@@ -12,3 +12,8 @@ resource "random_pet" "server_3" {
     length = var.pet_length
     prefix = var.pet_prefix
 }
+
+resource "random_pet" "server_4" {
+    length = var.pet_length
+    prefix = var.pet_prefix
+}
